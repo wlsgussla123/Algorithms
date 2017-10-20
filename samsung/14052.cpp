51047dcad2cadafba353838529b99d7ad11a9a9a
Jinhyeon Park <wlsgussla123@gmail.com>
2017-10-21 00:37:25 +0900

    삼성 기출 연구소, 재정비 후 다시 풀기

--- samsung/14052.cpp
+++ samsung/14052.cpp
@@ -1,161 +1,121 @@
 /*
 	작성자 : 박진현
-	문제 : 삼성 기출, 14502 연구소
-	
-	설명 : 사실 그 전에 모의고사로 시간을 재면서 풀었는데 실패했다. 
-	내가 시도했던 접근법은 바이러스로부터 경로를 모두 체크하고 각 경로마다
-	퍼뜨리는 바이러스 개수를 구하였다. 그 중에서 가장 큰 세 개를 뽑아서
-	벽을 두는 것으로 구현을 했으나, 문제는 같은 바이러스가 3개를 넘어가면 문제였다.
-
-	검색을 해서 코드를 보니 아예 벽을 모든 좌표에 넣어봄으로써 시도하는 것이 맞는 방향인가보다.
-
-	물론 이것은 MAX_SIZE가 작기 때문에 가능한 방법이다.
+	문제 : 삼성 기출,연구소
 */
-
 #include <iostream>
-#include <vector>
 #include <queue>
-
-#define MAX_SIZE 8
 
 using namespace std;
 
-// input
-int M, N;
-int map[MAX_SIZE][MAX_SIZE] = { 0, };
-int answer_map[MAX_SIZE][MAX_SIZE] = { 0, };
+int N, M;
 
-int visit[MAX_SIZE][MAX_SIZE] = { 0, };
-// 동서남북
-int dir[4][2] = { {0,1}, {0,-1}, {1,0}, {-1,0} };
-int answer = -1;
-vector<pair<int, int>> virus;
+int map[8][8] = { 0, };
+int visit[8][8] = { 0, };
+int answer = 0;
 
-int max(int a, int b) {
-	return a > b ? a : b;
-}
+int dirs[4][2] = { {0,1}, {0,-1}, {1,0}, {-1,0} };
 
-int countSafe() {
-	int cnt = 0;
+queue<pair<int, int>> q;
 
-	for (int i = 0; i < M; i++) {
-		for (int j = 0; j < N; j++) {
-			if (!answer_map[i][j]) cnt++;
-		}
-	}
-
-	return cnt;
-}
-
-// 기둥 세우고 다시 원래로 복원.
-void recoveryMap(int (*map)[MAX_SIZE], int(*temp)[MAX_SIZE]) {
-	for (int i = 0; i < M; i++) {
-		for (int j = 0; j < N; j++) {
-			map[i][j] = temp[i][j];
-		}
-	}
-}
-
-// 임시 복사
-void copyMap(int (*a)[MAX_SIZE], int (*b)[MAX_SIZE]) {
-	for (int i = 0; i < M; i++) {
-		for (int j = 0; j < N; j++) {
+void copy_map(int a[8][8], int b[8][8]) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
 			a[i][j] = b[i][j];
 		}
 	}
 }
 
-// 기둥을 세워둔 것에 기반하여 바이러스를 퍼뜨려 본다.
-void bfs() {
-	queue<pair<int, int>> q;
-	for (int i = 0; i < virus.size(); i++) q.push(virus[i]);
+int count_virus(int a[8][8]) {
+	int count = 0;
 
-	while (!q.empty()) {
-		int x = q.front().first;
-		int y = q.front().second;
-		q.pop();
-
-		for (int i = 0; i < 4; i++) {
-			int nx = x + dir[i][0];
-			int ny = y + dir[i][1];
-
-			if (nx >= 0 && nx < M && ny >= 0 && ny < N) {
-				if (!answer_map[nx][ny]) {
-					answer_map[nx][ny] = 2;
-					q.push(make_pair(nx, ny));
-				}
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (!a[i][j]) {
+				count++;
 			}
 		}
 	}
+
+	return count;
 }
 
-// cnt = 벽의 개수, 벽놓기 함수
-void dfs(int row, int col, int cnt) {
-	answer_map[row][col] = 1;
-	visit[row][col] = 1;
-
-	if (cnt == 3) {
-		//// 복사 맵이 필요 (기둥을 놓고 다시 복원을 해야하므로)
-		int temp[MAX_SIZE][MAX_SIZE];
-		copyMap(temp, answer_map);
-
-		bfs(); // bfs를 하면 바이러스를 다 퍼뜨리므로, 바이러스를 퍼뜨리기 전의 맵을 저장하는 temp 맵이 필요
-	
-		answer = max(answer, countSafe());
-		recoveryMap(answer_map, temp);
+// 바이러스 퍼뜨리기.
+void bfs() {
+	// 바이러스 좌표 일단 넣어두고.
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (map[i][j] == 2) {
+				q.push({ i, j });
+			}
+		}
 	}
-	else {
-		for (int i = row; i < M; i++) {
-			for (int j = 0; j < N; j++) {
-				if (!visit[i][j] && !answer_map[i][j]) {
-					dfs(i, j, cnt + 1);
+
+	int temp[8][8]; 
+	copy_map(temp, map);
+
+	while (!q.empty()) {
+		int row = q.front().first;
+		int col = q.front().second;
+		q.pop();
+		
+		for (int i = 0; i < 4; i++) {
+			int x = row + dirs[i][0];
+			int y = col + dirs[i][1];
+
+			if (x >= 0 && x < N && y >= 0 && y < M) {
+				if (!temp[x][y]) {
+					temp[x][y] = 2;
+					q.push({ x,y });
 				}
 			}
 		}
 	}
 
-	answer_map[row][col] = 0;
-	visit[row][col] = 0;
+	int count = count_virus(temp);
+	answer = max(answer, count);
+}
+
+// 백트랙킹으로 세 개씩 세울 때마다 bfs 바이러스 퍼뜨려보기
+void dfs(int row, int col, int cnt) {
+	if (cnt == 3) {
+		bfs();
+	}
+	else {
+		for (int i = row; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (!visit[i][j] && !map[i][j]) {
+					map[i][j] = 1;
+					visit[i][j] = 1;
+					dfs(i, j, cnt + 1);
+					map[i][j] = 0;
+					visit[i][j] = 0;
+				}
+			}
+		}
+	}
 }
 
 int main(void) {
-	scanf("%d %d", &M, &N);
-
-	for (int i = 0; i < M; i++) {
-		for (int j = 0; j < N; j++) {
+	scanf("%d %d", &N, &M);
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
 			scanf("%d", &map[i][j]);
-
-			answer_map[i][j] = map[i][j];
-			if (map[i][j] == 2) virus.push_back(make_pair(i,j));
 		}
 	}
 
-	for (int i = 0; i < M; i++) {
-		for (int j = 0; j < N; j++) {
-			if (!answer_map[i][j]) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (!visit[i][j] && !map[i][j]) {
+				map[i][j] = 1;
+				visit[i][j] = 1;
 				dfs(i, j, 1);
+				visit[i][j] = 0;
+				map[i][j] = 0;
 			}
 		}
 	}
 
-	printf("%d\n", answer);
+	cout << answer << endl;
 
 	return 0;
 }
-
-/*
-7 7
-2 0 0 0 1 1 0
-0 0 1 0 1 2 0
-0 1 1 0 1 0 0
-0 1 0 0 0 0 0
-0 0 0 0 0 1 1
-0 1 0 0 0 0 0
-0 1 0 0 0 0 0
-
-4 4
-2 0 0 1
-0 0 2 0
-0 1 0 0
-0 0 1 1
-*/
