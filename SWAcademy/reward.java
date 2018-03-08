abeb10af5294879b1a43a62392c2db0a1f09dd3a
Jinhyeon Park <wlsgussla123@gmail.com>
2018-03-08 15:08:44 +0900

    sw academy 달란트2

---
+++ SWAcademy/reward.java
@@ -0,0 +1,54 @@
+package algo;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+class Task {
+	private int N,P;
+	
+	private BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private StringTokenizer st = null;
+	private StringTokenizer getStringTokenizer() throws IOException {
+		return new StringTokenizer(br.readLine(), " ");
+	}
+	
+	private void input() throws IOException {
+		st = getStringTokenizer();
+		N = Integer.parseInt(st.nextToken());
+		P = Integer.parseInt(st.nextToken());
+	}
+	
+	public void run() throws IOException {
+		st = getStringTokenizer();
+		int T = Integer.parseInt(st.nextToken());
+		int idx = 1;
+		while(idx <= T) {
+			input();
+			int q = N/P;
+			int r = N%P;
+			long answer = 1;
+			while(P-- > 0) {
+				if(r!=0) {
+					answer *= (long)(q+1);
+					r--;
+				} else {
+					answer *= (long)q;
+				}
+			}
+			System.out.println("#"+idx+" "+answer);
+			idx++;
+		}
+	}
+}
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Task task = new Task();
+		task.run();
+	}
+}
