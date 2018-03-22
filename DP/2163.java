bf086c456423862ba81e088a1c8792172eec3aa7
Jinhyeon Park <wlsgussla123@gmail.com>
2018-03-22 18:05:09 +0900

    백준 2163번, 초콜릿 자르기

---
+++ DP/2163.java
@@ -0,0 +1,36 @@
+package algo;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+class Task {
+	private int N,M;
+	private StringTokenizer st = null;
+	private BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private StringTokenizer getStringTokenizer() throws IOException {
+		return new StringTokenizer(br.readLine(), " ");
+	}
+	
+	private void input() throws IOException {
+		st = getStringTokenizer();
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		System.out.println(N*M-1);
+	}
+	
+	public void run() throws IOException {
+		input();
+	}
+}
+
+public class Main {
+    public static void main(String a[]) throws IOException {
+    	Task task = new Task();
+    	task.run();
+    }
+}
