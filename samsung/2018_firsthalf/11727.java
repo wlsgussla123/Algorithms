df033ad85fc4b420acb4682cc859f0c279581c84
Jinhyeon Park <wlsgussla123@gmail.com>
2018-04-12 15:26:31 +0900

    2xn 타일링 2

---
+++ samsung/2018_firsthalf/11727.java
@@ -0,0 +1,54 @@
+package algo;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String args[]) throws IOException {
+		new Task().run();
+	}
+	
+	static class Task {
+		private int N;
+		private int[] dp;
+		
+		private StringTokenizer st = null;
+		private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		private BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		private StringTokenizer getStringTokenizer() throws IOException {
+			return new StringTokenizer(br.readLine(), " ");
+		}
+		
+		private void input() throws IOException {
+			st = getStringTokenizer();
+			N = Integer.parseInt(st.nextToken());
+			dp = new int[1005];
+		}
+		
+		private int solution() {
+			dp[1] = 1;
+			dp[2] = 3;
+			dp[3] = 5;
+			for(int i=4; i<=N; i++) {
+				dp[i] = (dp[i-2] * 2 + dp[i-1]) % 10007;
+			}
+			
+			return dp[N] % 10007;
+		}
+		
+		public void run() throws IOException {
+			input();
+			bw.write(String.valueOf(solution()));
+			close();
+		}
+
+		private void close() throws IOException {
+			bw.close();
+			br.close();
+		}
+	}
+}
