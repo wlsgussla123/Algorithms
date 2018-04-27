4e96c2cdb0aaf73e8c202f5f5fb899907e72f010
Jinhyeon Park <wlsgussla123@gmail.com>
2018-04-27 20:07:41 +0900

    직사각형에서 탈출

---
+++ mathematics/1085.java
@@ -0,0 +1,50 @@
+package algo;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		new Task().run();
+	}
+	
+	static class Task {
+		private int x,y,w,h;
+		
+		private BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		private StringTokenizer st = null;
+		private StringTokenizer getStringTokenizer() throws IOException {
+			return new StringTokenizer(br.readLine(), " ");
+		}
+		
+		private void input() throws IOException {
+			st = getStringTokenizer();
+			x = Integer.parseInt(st.nextToken());
+			y = Integer.parseInt(st.nextToken());
+			w = Integer.parseInt(st.nextToken());
+			h = Integer.parseInt(st.nextToken());
+		}
+		
+		private void solve() throws IOException {
+			int d1 = Math.min(x, Math.abs(x-w));
+			int d2 = Math.min(y, Math.abs(y-h));
+			bw.write(String.valueOf(Math.min(d1, d2)));
+		}
+		
+		public void run() throws IOException {
+			input();
+			solve();
+			close();
+		}
+		
+		private void close() throws IOException {
+			bw.close();
+			br.close();
+		}
+	}
+}
