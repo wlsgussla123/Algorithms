90df110c6dfcc641e74cb64182601d64470b77ed
Jinhyeon Park <wlsgussla123@gmail.com>
2018-05-08 20:19:58 +0900

    백준 11722번, 가장 긴 감소하는 부분 수열

---
+++ DP/11722.java
@@ -0,0 +1,66 @@
+package algo;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		new Task().run();
+	}
+	
+	static class Task {
+		private int N;
+		private int[] arr = new int[1001];
+		private int[] dp = new int[1001];
+		
+		private BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		private StringTokenizer st = null;
+		private StringTokenizer getStringTokenizer() throws IOException {
+			return new StringTokenizer(br.readLine(), " ");
+		}
+		
+		private void input() throws IOException {
+			st = getStringTokenizer();
+			N = Integer.parseInt(st.nextToken());
+			st = getStringTokenizer();
+			for(int i=1; i<=N; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			for(int i=1; i<=N; i++) {
+				dp[i] = -1;
+			}
+		}
+		
+		private int solve() {
+			int ans = 1;
+			for(int i=1; i<=N; i++) {
+				dp[i] = 1;
+				for(int j=1; j<=i-1; j++) {
+					if(arr[i] < arr[j] && dp[j] + 1 > dp[i]) {
+						dp[i] = dp[j] + 1;
+						ans = ans > dp[i] ? ans : dp[i];
+					}
+				}
+			}
+			
+			return ans;
+		}
+		
+		public void run() throws IOException {
+			input();
+			bw.write(String.valueOf(solve()));
+			close();
+		}
+		
+		private void close() throws IOException {
+			bw.close();
+			br.close();
+		}
+	}
+}
