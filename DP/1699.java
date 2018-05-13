35a4ec9060b1f107482c58aa4a357f832cce4056
Jinhyeon Park <wlsgussla123@gmail.com>
2018-05-13 11:43:33 +0900

    백준 1699번, 제곱수의 합

---
+++ DP/1699.java
@@ -0,0 +1,61 @@
+package algo;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		new Task().run();
+	}
+	
+	static class Task {
+		private int N;
+		private int[] dp = new int[100001];
+		
+		private BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		private StringTokenizer st = null;
+		private StringTokenizer getStringTokenizer() throws IOException {
+			return new StringTokenizer(br.readLine(), " ");
+		}
+		
+		private void input() throws IOException {
+			st = getStringTokenizer();
+			N = Integer.parseInt(st.nextToken());
+		}
+		
+		private void solve() throws IOException {
+			for(int i=1; i<=N; i++) {
+				dp[i] = Integer.MAX_VALUE;
+				for(int j=1; j*j <= i; j++) {
+					if(dp[i] > dp[i - j*j] + 1 || dp[i] == 0) {
+						dp[i] = dp[i - j*j] + 1;
+					}
+				}
+			}
+			System.out.println(dp[N]);
+		}
+		
+		public void run() throws IOException {
+			input();
+			solve();
+			close();
+		}
+		
+		private void close() throws IOException {
+			bw.close();
+			br.close();
+		}
+	}
+}
