67b8f4dcb60cacc2a0efd403890a57b228a18a05
Jinhyeon Park <wlsgussla123@gmail.com>
2018-05-19 09:14:08 +0900

    백준 1904번, 01타일

---
+++ DP/1904.java
@@ -0,0 +1,59 @@
+package algo;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		new Task().run();
+	}
+	
+	static class Task {
+		private int N;
+		private int[] dp = new int[1000001];
+		
+		private BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		private StringTokenizer st = null;
+		private StringTokenizer getStringTokenizer() throws IOException {
+			return new StringTokenizer(br.readLine(), " ");
+		}
+		
+		private void input() throws IOException {
+			st = getStringTokenizer();
+			N = Integer.parseInt(st.nextToken());
+		}
+		
+		private void solve() throws IOException {
+			dp[1] = 1;
+			dp[2] = 2;
+			for(int i=3; i<=N; i++) {
+				dp[i] = (dp[i-1] + dp[i-2]) % 15746;
+			}
+			
+			bw.write(String.valueOf(dp[N]));
+		}
+		
+		public void run() throws IOException {
+			input();
+			solve();
+			close();
+		}
+		
+		private void close() throws IOException {
+			bw.close();
+			br.close();
+		}
+	}
+}
