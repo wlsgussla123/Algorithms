ef19e8c4ee2631e5a0bff3e03def7a27ebec800d
Jinhyeon Park <wlsgussla123@gmail.com>
2018-05-30 12:03:03 +0900

    백준 8320번, 직사각형을 만드는 방법

---
+++ figure/8320.java
@@ -0,0 +1,49 @@
+package algo;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		new Solution().run();
+	}
+
+	static class Solution {
+		private int N;
+		
+		private BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		private StringTokenizer st = null;
+		private StringTokenizer getStringTokenizer() throws IOException {
+			return new StringTokenizer(br.readLine(), " ");
+		}
+
+		private void input() throws IOException {
+			st = getStringTokenizer();
+			N = Integer.parseInt(st.nextToken());
+			
+			int cnt = 0;
+			for(int i=1; i<=N; i++) {
+				for(int j=i; i*j<=N; j++) {
+					cnt++;
+				}
+			}
+			System.out.println(cnt);
+		}
+
+		public void run() throws IOException {
+			input();
+			close();
+		}
+
+		private void close() throws IOException {
+			bw.close();
+			br.close();
+		}
+	}
+}
